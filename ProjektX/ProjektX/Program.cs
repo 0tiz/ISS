29ece3ce41c48f78cedbd7227e2be43ba27a55b5
0tiz <0tiz@user.noreply.github.com>
2018-06-14 22:54:28 +0200

    peoplesInSpace

--- ProjektX/ProjektX/Program.cs
+++ ProjektX/ProjektX/Program.cs
@@ -15,19 +15,32 @@ namespace ISS
             using (var webClient = new System.Net.WebClient())
             {
                 var issTracking = webClient.DownloadString("http://api.open-notify.org/iss-now.json");
+                var peopleInSpace = webClient.DownloadString("http://api.open-notify.org/astros.json");
                 //example {"iss_position": {"latitude": "1.3102", "longitude": "4.2245"}, "timestamp": 1528496959, "message": "success"}
 
                 var issData = JsonConvert.DeserializeObject<IssData>(issTracking);
                 var onCheck = JsonConvert.DeserializeObject<OnCheck>(issTracking);
+                var peoples = JsonConvert.DeserializeObject<SpaceCowboys>(peopleInSpace);
                 var onlineCheck = GetOnlineCheck(onCheck.Message);
 
-                //string openMaps = string.Format(System.Globalization.CultureInfo.InvariantCulture.NumberFormat, "www.google.com/maps/place/{0},{1}", issData.IssPosition.Latitude, issData.IssPosition.Longitude);
-                //System.Diagnostics.Process.Start(openMaps);
+                string openMaps = string.Format(System.Globalization.CultureInfo.InvariantCulture.NumberFormat, "www.google.com/maps/place/{0},{1}", issData.IssPosition.Latitude, issData.IssPosition.Longitude);
+                
 
                 Console.WriteLine(onlineCheck);
                 Console.WriteLine(issData.IssPosition.Latitude);
                 Console.WriteLine(issData.IssPosition.Longitude);
-                Console.ReadLine();
+                Console.WriteLine("Currently are "+peoples.number+" peoples on the ISS ");
+                Console.WriteLine("Do you want to see the postition from the ISS on Google Maps? Type YES or NO is the console");
+             
+                string userInput = Console.ReadLine();
+                if (userInput == "YES")
+                {
+                    System.Diagnostics.Process.Start(openMaps);
+                }
+                else
+                {
+                    Console.WriteLine("OK");
+                }
 
 
 
@@ -53,6 +66,10 @@ namespace ISS
         {
             public decimal Latitude;
             public decimal Longitude;
+        }
+        class SpaceCowboys
+        {
+            public int number;
         }
 
         public static string GetOnlineCheck(string onlineCheck)
